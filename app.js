d9fb0acaf8fc960a7053ca9fe914a033624971ad
Arifcan Yılmaz <wisemann1917@gmail.com>
2024-10-17 13:10:46 +0300

    cleanblog odev3

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
-const ejs = require('ejs')
-const express = require('express');
-const app = express();
+const path = require("path"); //Dosya yolunu belirtir. (CORE)
+const exp = require('constants'); //Node.js'teki sabitlere erişmek için (CORE)
+
+const ejs = require("ejs"); //Gömülü js dosyaları (NPM)
+const express = require("express"); // Express (NPM)
+const mongoose = require('mongoose') // Mongoose (NPM)
+
+const Blog = require('./models/Blog') //photo modelimizi import ettik.
+
+const app = express(); // Express (NPM)
+
+// connect DB
+mongoose.connect('mongodb://localhost/cleanblog-db')
+    .then(() => console.log('veritabanına bağlandı'))
 
 //TEMPLATE ENGINE
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 // MIDDLEWARES
-app.use(express.static('public'))
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 //ROUTER
-app.get('/', (req, res) => {
-  res.render("index")
+app.get("/", async (req, res) => {
+  const blogItem = await Blog.find({})
+
+  res.render("index", {
+    blogItem
+  });
 });
-app.get('/about', (req, res) => {
-  res.render("about")
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
-app.get('/post', (req, res) => {
-  res.render("post")
+
+app.get("/post/:id", async (req, res) => {
+  const postId = req.params.id
+  const postItem = await Blog.findById(postId)
+  res.render('post', {
+    postItem
+  })
 });
-app.get('/add_post', (req, res) => {
-  res.render("add_post")
+
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
 });
+
+app.post("/blogs", async (req,res) => {
+  await Blog.create(req.body)
+  res.redirect('/')
+})
+
 
 
 const port = 3000;
